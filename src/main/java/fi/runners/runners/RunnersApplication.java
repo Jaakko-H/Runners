9d99a6aa808e59a96bb75f3d1dbc8504ee4cfe0e
Jaakko <jaheikela@gmail.com>
2017-06-23 19:57:09 +0300

    Add auto configuration.

--- src/main/java/fi/runners/runners/RunnersApplication.java
+++ src/main/java/fi/runners/runners/RunnersApplication.java
@@ -1,11 +1,14 @@
 package fi.runners.runners;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.*;
+import org.springframework.boot.autoconfigure.orm.jpa.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class RunnersApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RunnersApplication.class, args);
 	}
